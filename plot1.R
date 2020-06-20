71eeaa2b3fa516ab8a6cd9f103bc750a81cb1997
CodeR <64930083+rajcpandey@users.noreply.github.com>
2020-06-20 15:54:34 +0100

    V 1.0 - 20/06/2020

---
+++ plot1.R
@@ -0,0 +1,36 @@
+# File Name         plot1.R
+# Developed by      Raj Kumar Pandey
+# Date              20/06/2020
+# Assignment        Plotting Assignment 1 for Exploratory Data Analysis
+#-------------------------------------------------------------------------------------------------------------------------------
+# Loading required packages and libraries
+  
+#install.packages(sqldf)
+#install.packages("tidyverse")
+library(sqldf)
+library(tidyverse)
+
+  
+# convert file to csv format so that we can use it with read.csv.sql function and only select the rows we want
+# We have only one file in the working directory
+
+files <- list.files(pattern="*.txt")
+newfiles <- gsub(".txt$", ".csv", files)
+file.rename(files, newfiles)
+
+
+# Read on selected data from the txt file
+loadData <- read.csv2.sql("household_power_consumption.csv", sql = "select * from file where Date IN ('1/2/2007','2/2/2007')")
+
+
+#Convert the date and time from character to proper date and time
+loadData <- loadData %>%  mutate(Date = as.Date(Date, format = "%d/%m/%Y"), Time = strptime(Time, format = "%H:%M:%S"))
+
+
+# Define device, set characteristics and plot
+png("plot1.png", 480, 480)
+
+hist(loadData$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+
+# set the device off and back to the default screen device
+dev.off()
